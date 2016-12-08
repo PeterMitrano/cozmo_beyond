f20a3a55d0c88de3df233181ea93ba4cd28cb2ed
Peter Mitrano <mitranopeter@gmail.com>
2016-12-07 21:24:06 -0500

    cubes are found in different colors
    look-around generator works

--- cozmo_shuffle.py
+++ cozmo_shuffle.py
@@ -20,20 +20,24 @@ class States:
     DONE = "DONE"
 
 
-async def turn_to_small_random_angle(robot : cozmo.robot.Robot):
-    new_pose = robot.pose
-    d_angle = random.randint(-6, 6) * 5 # from -30 to 30 by 5's
-    print(new_pose)
-    print(cozmo.util.pose_z_angle(0, 0, 0, degrees(d_angle)))
-    await robot.go_to_pose(new_pose, relative_to_robot=False).wait_for_completed()
+def small_random_angle(min_deg=-30, max_deg=30, step=2):
+    current_angle = 0
+    while True:
+        if current_angle >= 0:
+            r = random.randrange(min_deg, -step, step)
+        else:
+            r = random.randrange(step, max_deg, step)
+        turn_angle = r - current_angle
+        current_angle += turn_angle
+        yield turn_angle
 
 
 def new_image_handler(evt, obj=None, tap_count=None, **kwargs):
     pass
 
-
 async def run(sdk_conn):
     robot = await sdk_conn.wait_for_robot()
+    print("BATTERY LEVEL: %f" % robot.battery_voltage)
 
     # setup camera
     robot.camera.image_stream_enabled = True
@@ -46,15 +50,21 @@ async def run(sdk_conn):
         print(state)
         if state == States.LOOKING_FOR_CUBES:
             cubes_found = []
+            cube_colors = [cozmo.lights.green_light, cozmo.lights.blue_light, cozmo.lights.red_light]
             while True:
+
+                # check for cubes
                 cube = await robot.world.wait_for_observed_light_cube(timeout=30, include_existing=False)
                 if cube not in cubes_found:
                     print("found %s" % cube)
                     cubes_found.append(cube)
-                    cube.set_lights(cozmo.lights.green_light)
+
+                    # indicate to friend that we found the blocks
+                    cube.set_lights(cube_colors.pop())
                     found_block_anim = robot.play_anim_trigger(cozmo.anim.Triggers.BlockReact)
                     await found_block_anim.wait_for_completed()
 
+                # we're done once we have all three (unique) cubes
                 if len(cubes_found) == 3:
                     state = States.READY_ANIM
                     break;
@@ -65,6 +75,11 @@ async def run(sdk_conn):
         elif state == States.WATCHING:
             await robot.play_anim("anim_hiking_edgesquintgetin_01").wait_for_completed()
 
+            s_gen = small_random_angle()
+            for i in range(5):
+                angle = next(s_gen)
+                await robot.turn_in_place(degrees(angle)).wait_for_completed()
+            print("done.")
             state = States.THINKING
         else:
             print("giving up.")

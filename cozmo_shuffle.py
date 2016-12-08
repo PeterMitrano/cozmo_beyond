c7a99b206f2f508014fe3ef65103b04120defc46
Peter Mitrano <mitranopeter@gmail.com>
2016-12-07 20:14:07 -0500

    succesfully finds cubes andl lights them up
    doesn't look around though

--- cozmo_shuffle.py
+++ cozmo_shuffle.py
@@ -20,6 +20,14 @@ class States:
     DONE = "DONE"
 
 
+async def turn_to_small_random_angle(robot : cozmo.robot.Robot):
+    new_pose = robot.pose
+    d_angle = random.randint(-6, 6) * 5 # from -30 to 30 by 5's
+    print(new_pose)
+    print(cozmo.util.pose_z_angle(0, 0, 0, degrees(d_angle)))
+    await robot.go_to_pose(new_pose, relative_to_robot=False).wait_for_completed()
+
+
 def new_image_handler(evt, obj=None, tap_count=None, **kwargs):
     pass
 
@@ -30,28 +38,32 @@ async def run(sdk_conn):
     # setup camera
     robot.camera.image_stream_enabled = True
     robot.camera.add_event_handler(cozmo.robot.camera.EvtNewRawCameraImage, new_image_handler)
+    await robot.set_head_angle(degrees(0)).wait_for_completed()
+    robot.move_lift(-1)
 
     state = States.LOOKING_FOR_CUBES
-    cubes = []
     while True:
         print(state)
         if state == States.LOOKING_FOR_CUBES:
-            cubes = await robot.world.wait_until_observe_num_objects(num=3, object_type=cozmo.objects.LightCube, timeout=10)
-            if len(cubes) == 3:
-                state = States.READY_ANIM
+            cubes_found = []
+            while True:
+                cube = await robot.world.wait_for_observed_light_cube(timeout=30, include_existing=False)
+                if cube not in cubes_found:
+                    print("found %s" % cube)
+                    cubes_found.append(cube)
+                    cube.set_lights(cozmo.lights.green_light)
+                    found_block_anim = robot.play_anim_trigger(cozmo.anim.Triggers.BlockReact)
+                    await found_block_anim.wait_for_completed()
+
+                if len(cubes_found) == 3:
+                    state = States.READY_ANIM
+                    break;
+
         elif state == States.READY_ANIM:
             await robot.play_anim("anim_speedtap_findsplayer_01").wait_for_completed()
             state = States.WATCHING
         elif state == States.WATCHING:
             await robot.play_anim("anim_hiking_edgesquintgetin_01").wait_for_completed()
-
-            initial_pose = robot.pose
-            for i in range(5):
-                new_pose = initial_pose
-                d_angle = random.randint(-6, 6) * 5 # from -30 to 30 by 5's
-                print(d_angle)
-                new_pose += cozmo.util.pose_z_angle(0, 0, 0, d_angle)
-                await robot.go_to_pose(new_pose, relative_to_robot=False).wait_for_completed()
 
             state = States.THINKING
         else:

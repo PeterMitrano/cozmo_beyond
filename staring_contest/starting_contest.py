ebe7307dc0fc6e07bd049c60dc2115d0283ba3ef
Peter Mitrano <mitranopeter@gmail.com>
2016-12-11 21:42:22 -0500

    ROI extraction works

--- staring_contest/starting_contest.py
+++ staring_contest/starting_contest.py
@@ -2,30 +2,68 @@ import sys
 import cozmo
 import asyncio
 import cv2
+from PIL import ImageDraw, ImageColor
+
 import BlinkPipeline
 
 
-class StaringContest:
+class Pt():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+    def __round__(self):
+        self.x = round(self.x)
+        self.y = round(self.y)
+
+    def __repr__(self):
+        return "%i, %i" % (self.x, self.y)
+
+
+class StaringContest(cozmo.annotate.Annotator):
     def __init__(self):
         # order: top left x, top left y, bottom right x, bottom right y
         # top left pixel is 0,0
-        self.eye_region_of_interest = [0, 0, 320, 240]
+        self.eye_region_of_interest = [Pt(0, 0), Pt(320, 240)]
+        self.enabled = True
 
     def new_image_handler(self, evt, obj=None, tap_count=None, **kwargs):
-        roi = evt.image.crop(*self.eye_region_of_interest)
-        roi.save('roi.png', "PNG")
+        bounds = (self.eye_region_of_interest[0].x, self.eye_region_of_interest[0].y,
+                  self.eye_region_of_interest[1].x, self.eye_region_of_interest[1].y)
+        roi = evt.image.crop(bounds)
+        # print ("saved image")
+        # roi.save('roi.png', "PNG")
 
     @cozmo.event.filter_handler(cozmo.faces.EvtFaceObserved)
     def observed_face_handler(self, evt, obj=None, tap_count=None, **kwargs):
-        self.eye_region_of_interest = [evt.face.left_eye[0][0], evt.face.left_eye[0][1], evt.face.right_eye[2][0],
-                                       evt.face.right_eye[2][1]]
+        # when we get a new face detection, save the area around the eyes
+        eye_padding = 20
+        scale = 2.15
+        if len(evt.face.left_eye) > 2 and len(evt.face.right_eye) > 2:
+            roi_x1 = max(evt.face.left_eye[0].x - eye_padding, 0) * scale
+            roi_y1 = max(evt.face.left_eye[0].y - eye_padding, 0) * scale
+            roi_x2 = min(evt.face.right_eye[2].x + eye_padding, 320) * scale
+            roit_y2 = min(evt.face.right_eye[2].y + eye_padding, 240) * scale
+            self.eye_region_of_interest = [Pt(roi_x1, roi_y1), Pt(roi_x2, roit_y2)]
 
     async def run(self, sdk_conn: cozmo.conn.CozmoConnection):
         robot = await sdk_conn.wait_for_robot()
+        robot.world.image_annotator.annotation_enabled = True
         robot.add_event_handler(cozmo.faces.EvtFaceObserved, self.observed_face_handler)
         robot.camera.image_stream_enabled = True
         robot.camera.add_event_handler(cozmo.robot.camera.EvtNewRawCameraImage, self.new_image_handler)
+        robot.world.image_annotator.add_annotator('roi', self)
+
+        while True:
+            await asyncio.sleep(1)
+
+    def apply(self, image, scale):
+        blue = ImageColor.getrgb("#f00")
+        roi = [Pt(self.eye_region_of_interest[0].x, self.eye_region_of_interest[0].y),
+            Pt(self.eye_region_of_interest[1].x, self.eye_region_of_interest[0].y),
+            Pt(self.eye_region_of_interest[1].x, self.eye_region_of_interest[1].y),
+        Pt(self.eye_region_of_interest[0].x, self.eye_region_of_interest[1].y)]
+        cozmo.annotate.add_polygon_to_image(image, roi, 1, blue)
 
 
 if __name__ == "__main__":
@@ -33,6 +71,6 @@ if __name__ == "__main__":
 
     try:
         sc = StaringContest()
-        cozmo.connect(sc.run)
+        cozmo.connect_with_tkviewer(sc.run)
     except cozmo.ConnectionError as e:
         sys.exit("Connection error: %s" % e)

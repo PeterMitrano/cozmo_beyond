b0c86ffbcb0fe97deefe406d0591b2172d8808d9
Peter Mitrano <mitranopeter@gmail.com>
2016-12-17 15:39:10 -0500

    Cube Shuffle v1.1.0
    added music and cube flip to shuffle game

--- cube_shuffle/cube_shuffle.py
+++ cube_shuffle/cube_shuffle.py
@@ -12,6 +12,7 @@ from math import cos
 from math import sin
 from math import acos
 from math import sqrt
+import pygame
 
 
 def length(v):
@@ -45,6 +46,11 @@ def angle_to_cube(robot_pose, cube):
     cube_vector = (cube.pose.position.x - robot_pose.position.x, cube.pose.position.y - robot_pose.position.y)
     robot_vector = (cos(robot_pose.rotation.angle_z.radians), sin(robot_pose.rotation.angle_z.radians))
     return angle_clockwise(cube_vector, robot_vector)
+
+
+def distance_to_cube(robot, cube):
+    cube_vector = (cube.pose.position.x - robot.pose.position.x, cube.pose.position.y - robot.pose.position.y)
+    return length(cube_vector)
 
 
 # Make sure World knows how to instantiate the subclass
@@ -132,9 +138,45 @@ async def wait_for_three_cubes(robot: cozmo.robot.Robot, play_anim=False, show_c
         except asyncio.TimeoutError:
             # tell friend we're confused and look around a bit
             robot.abort_all_actions()
-            await robot.play_anim("anim_memorymatch_failhand_01").wait_for_completed()
+            await robot.play_anim_trigger(cozmo.anim.Triggers.RollBlockRealign).wait_for_completed()
             _, robot_angle = next(look_around_gen)
             await robot.turn_in_place(degrees(robot_angle)).wait_for_completed()
+
+
+async def flip_motion(robot):
+    await robot.set_lift_height(1, duration=0.2).wait_for_completed()
+    await robot.drive_wheels(40, 40, duration=1.2)
+    await robot.drive_wheels(-70, -70)
+    await robot.set_lift_height(0, duration=1.6).wait_for_completed()
+    await robot.drive_wheels(-70, -70, duration=0.5)
+    await robot.drive_wheels(0, 0)
+
+
+async def flip_cube(robot, cube):
+    dist_to_cube = 60
+    max_dist_to_cube = 70
+    while True:
+        await robot.set_lift_height(1, duration=0.2).wait_for_completed()
+        current_action = robot.go_to_object(cube, distance_mm(dist_to_cube))
+        await current_action.wait_for_completed()
+        if current_action.has_failed:
+            code, reason = current_action.failure_reason
+            d = distance_to_cube(robot, cube)
+            if d < max_dist_to_cube:  # good enough!
+                break
+            if reason == "Reached maximum retries for action":
+                await robot.drive_straight(distance_mm(-60), speed_mmps(100)).wait_for_completed()
+        elif current_action.has_succeeded:
+            break
+
+    await flip_motion(robot)
+    return
+
+
+async def quick_turn(robot, degrees):
+    ''' uses hacky dead reckoning '''
+    robot.drive_wheels(-70, -70)
+    await asyncio.sleep(1)
 
 
 async def run(sdk_conn):
@@ -142,9 +184,13 @@ async def run(sdk_conn):
     print("BATTERY LEVEL: %f" % robot.battery_voltage)
 
     await robot.set_head_angle(degrees(0)).wait_for_completed()
-    robot.move_lift(-1)
+    await robot.set_lift_height(0, duration=0.2).wait_for_completed()
 
-    correct_guess_rate = 0.50
+    # setup the audio
+    pygame.mixer.init()
+    pygame.mixer.music.load('assets/shuffling_music.wav')
+
+    correct_guess_rate = 0.40
 
     state = States.LOOKING_FOR_CUBES
     cubes = []
@@ -183,26 +229,21 @@ async def run(sdk_conn):
 
         elif state == States.READY_ANIM:
             # tell friend we're ready with an animation, and by flashing the cubes
+            await robot.play_anim("anim_speedtap_findsplayer_01").wait_for_completed()
+            pygame.mixer.music.play()
             for cube in cubes:
                 cube.set_light(cozmo.lights.white_light)
-            await robot.play_anim("anim_speedtap_findsplayer_01").wait_for_completed()
-
             state = States.WATCHING
 
         elif state == States.WATCHING:
-
-            await robot.play_anim("anim_hiking_edgesquintgetin_01").wait_for_completed()
-
             # turn around a bit while we watch
             look_around_gen = small_random_angle()
             current_angle = 0
             for i in range(3):
                 current_angle, robot_angle = next(look_around_gen)
                 await robot.turn_in_place(degrees(robot_angle)).wait_for_completed()
-
             # turn back to center
             await robot.turn_in_place(degrees(-current_angle)).wait_for_completed()
-
             state = States.REFINDING_CUBES
 
         elif state == States.REFINDING_CUBES:
@@ -214,6 +255,9 @@ async def run(sdk_conn):
             state = States.GUESSING
 
         elif state == States.GUESSING:
+            # show friend we're guessing
+            await robot.play_anim("anim_meetcozmo_lookface_02").wait_for_completed()
+
             # pick a cube
             if random.random() < correct_guess_rate:
                 guessed_cube = friend_cube
@@ -221,32 +265,34 @@ async def run(sdk_conn):
             else:
                 guessed_cube = non_friend_cubes[random.randint(0, 1)]
                 state = States.INCORRECT
-
-            turn_to_face_angle = angle_to_cube(robot.pose, guessed_cube)
-            await robot.turn_in_place(radians(turn_to_face_angle)).wait_for_completed()
-            await robot.play_anim("anim_pounce_long_01").wait_for_completed()
-            robot.move_head(1)
+            # go flip the cube
+            await flip_cube(robot, guessed_cube)
+            await asyncio.sleep(0.5)
 
         elif state == States.INCORRECT:
             for cube in cubes:
                 cube.set_light(cozmo.lights.red_light)
+            await robot.drive_wheels(-200, -200, duration=0.5)
             await robot.play_anim("anim_reacttocliff_stuckleftside_01").wait_for_completed()
             state = States.DONE
 
         elif state == States.CORRECT:
-            pre_celebration_pose = robot.pose
             for cube in cubes:
                 cube.start_light_chaser(cozmo.lights.green_light)
             await robot.play_anim("anim_speedtap_wingame_intensity02_01").wait_for_completed()
             for cube in cubes:
                 cube.stop_light_chaser()
-            await robot.go_to_pose(pre_celebration_pose, relative_to_robot=False).wait_for_completed()
             state = States.DONE
 
         elif state == States.DONE:
-            await asyncio.sleep(1)
-            return
-
+            print("PRESS ENTER TO PLAY AGAIN, press q then enter to quit")
+            key = input()
+            if key == 'q':
+                await asyncio.sleep(1)
+                return
+            else:
+                await asyncio.sleep(1)
+                state = States.LOOKING_FOR_CUBES
 
 if __name__ == '__main__':
     cozmo.setup_basic_logging()

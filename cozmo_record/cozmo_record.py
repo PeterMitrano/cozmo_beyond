0a0ec03b0bf4f91f0fe3b899c0e226f558546bab
Peter Mitrano <mitranopeter@gmail.com>
2017-12-20 00:30:17 -0500

    add drive-in-square functionality

--- cozmo_record/cozmo_record.py
+++ cozmo_record/cozmo_record.py
@@ -5,8 +5,11 @@ import cv2
 import cozmo
 import argparse
 
+from cozmo.util import distance_mm, degrees, speed_mmps
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--dont-save', action="store_true")
+parser.add_argument('--dont-save', action="store_true", help="don't write the video file")
+parser.add_argument('--square', action="store_true", help="drive in a square instead of sitting still")
 args = parser.parse_args()
 
 now = datetime.now()
@@ -36,7 +39,17 @@ def program(robot: cozmo.robot.Robot):
     robot.camera.image_stream_enabled = True
     robot.world.add_event_handler(cozmo.world.EvtNewCameraImage, new_image_handler)
 
-    input("press q to exit")
+    if args.square:
+        lift_up = robot.set_lift_height(1)
+        lift_up.wait_for_completed()
+        head_down = robot.set_head_angle(cozmo.robot.MIN_HEAD_ANGLE)
+        head_down.wait_for_completed()
+
+        for _ in range(4):
+            robot.drive_straight(distance_mm(150), speed_mmps(50)).wait_for_completed()
+            robot.turn_in_place(degrees(90)).wait_for_completed()
+    else:
+        input("press q to exit")
 
 
 cozmo.robot.Robot.drive_off_charger_on_connect = False
